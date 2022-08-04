0d0efc7034dc712553751f219c647e93fe598618
jm-moore <jmmoore9358@gmail.com>
2022-08-04 10:37:24 -0500

    Added user input option with given output

--- Program.cs
+++ Program.cs
@@ -1,4 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, how old are you?");
+Console.WriteLine("Hello, how old are you? Enter age below:");
+int age = Convert.ToInt32 (Console.ReadLine());
+if (age >= 18)
+    Console.WriteLine("You are an adult!");
+else
+    Console.WriteLine("You are a child!");
 
 
